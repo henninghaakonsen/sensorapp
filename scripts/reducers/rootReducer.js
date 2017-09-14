d0f039f75211ce3e6964c0d8e987aaf75f8fa33b
Henning Håkonsen <henninghaakonsen@me.com>
2017-09-14 20:58:25 +0200

    Moved files from sensorserver git

---
+++ scripts/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+// @flow
+
+import { combineReducers } from 'redux'
+import navigation from './navigationReducer'
+
+const rootReducer = combineReducers({
+  navigation,
+})
+
+export default rootReducer
